ad7be52b5940768668a8babe7862f1877ac99253
Jyotirmoy Paul <cipherresfeber@gmail.com>
2020-05-17 10:19:20 +0530

    added empty flutter app

---
+++ app_flutter/android/app/src/main/kotlin/com/paul/passwordlocker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.paul.passwordlocker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_flutter/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
